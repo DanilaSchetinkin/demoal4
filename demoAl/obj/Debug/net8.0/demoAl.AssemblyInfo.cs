fe4ba5794d9b5168dac7d68e9c03bb6c13bcc18a
DanilaSchetinkin <danya.sch2015@gmail.com>
2025-04-25 14:27:06 +0300

    front

--- demoAl/obj/Debug/net8.0/demoAl.AssemblyInfo.cs
+++ demoAl/obj/Debug/net8.0/demoAl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demoAl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c2267dbfe8c1d6faac1e12adccca68775134fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("demoAl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demoAl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
